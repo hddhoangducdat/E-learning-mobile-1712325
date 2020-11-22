96651a81a8f47e95f7f273acda0aa05feb7469ad
Hoàng Đức Đạt <noir@pop-os.localdomain>
2020-11-23 04:37:06 +0700

    UI home and menu

--- ELearning/src/components/ButtonForm.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Button, StyleSheet } from "react-native";
-
-export default function ButtonForm({ title, callBack }: any) {
-  return <Button onPress={callBack} title={title} color={} />;
-}
-
-const styles = StyleSheet.create({});
